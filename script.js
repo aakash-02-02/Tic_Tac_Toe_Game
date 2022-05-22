929f5bea5b84b442acf3b07ddffb9e818c33d5e4
aakash-02-02 <101527272+aakash-02-02@users.noreply.github.com>
2022-05-23 01:07:41 +0530

    2ndcommit

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ const checkWin = () => {
 
 // Game Logic
 
-// music.play()
+music.play()
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext');
